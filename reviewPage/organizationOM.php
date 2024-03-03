87aea96a8f448d6dda75bf3d5819cf294ea62887
RusiriniWijethunga <95708918+RusiriniWijethunga@users.noreply.github.com>
2024-03-03 20:22:38 +0530

    added review pages

---
+++ reviewPage/organizationOM.php
@@ -0,0 +1,184 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>SUJCS</title>
+  <link rel="stylesheet" href="organizationOM.css">
+</head>
+
+<body>
+  <nav class="navbar">
+    <div class="container">
+      <div class="logo">
+        <img src="D:/work/HTML/WEB_PROJECT/SUJCS_Web_Project/images/logo.png" alt="Logo">
+      </div>
+      <div class="menu">
+        <ul>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Call for Papers</a></li>
+          <li><a href="#">Review</a></li>
+          <li><a href="#">Guidelines</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </div>
+      <div class="buttons">
+        <a href="#" class="login-btn">Login</a>
+        <a href="#" class="signup-btn">Sign Up</a>
+      </div>
+    </div>
+  </nav>
+
+  <div class="title-container">
+    <h3>Find Articles</h3>
+    <div class="search-container">
+      <input type="text" placeholder="Search here">
+      <button type="submit">Search</button>
+    </div>
+  </div>
+
+  <body>
+    <h4 style="font-size: 16px;">Organization Of Manuscript</h4>
+    <p style="color: grey;" ,style="text-align: justify;">The manuscript should contain the items mentioned hereunder.
+    </p>
+    <p style="color: grey;" ,style="text-align: justify;">Title, List of authors together with affiliations</p>
+    <div class="list">
+      <ol style="color: grey;">
+        <li>Abstract & Keywords</li>
+        <li>Introduction</li>
+        <li>Literature Review</li>
+        <li>Materials & Methods</li>
+        <li>Results & Discussion</li>
+        <li>Conclusion</li>
+        <li>Acknowledgments (optional)</li>
+        <li>References</li>
+        <li>Annexures (optional)</li>
+      </ol>
+    </div>
+    <h5 style="font-size: 14px;">Language</h5>
+    <p style="color: grey;">The language of the manuscript must be in English (British standard).</p>
+    <h5 style="font-size: 14px;" ,style="color: grey;">Length of Paper</h5>
+    <p style="color: grey;">The paper's length must fall within the range of 7000 to 10000 words, encompassing the
+      references and annexures. Any submission<br>
+      exceeding 10000 words will be declined. Articles should be typed in 12-point Times New Roman font, with 1.5 line
+      spacing, on one side of<br>
+      A4 paper, maintaining normal margins (Top 1”, Bottom 1”, Left 1”, and Right 1”). Authors are encouraged to express
+      ideas concisely<br>
+      while ensuring clarity in their writing.</p>
+    <h5 style="font-size: 14px;">Title Page</h5>
+    <p style="color: grey;">The Title page is a separate page before the text. It should include the following
+      information:</p>
+    <div class="subtopics">
+      <p style="color:gray;" ,style="font-size:16;"><b>Title</b></p>
+      <p style="color:gray;">The title should be concise and informative. Abbreviations and formulae should be avoided
+        where possible.</p>
+      <p style="color:gray;" ,style="font-size:16;"><b>Author’s Names & Affiliations</b></p>
+      <p style="color:gray;">Please provide the author's initials followed by their last name. Include the authors'
+        affiliation addresses (the location<br>
+        where the research was conducted) beneath their names. Use lowercase superscript letters to denote each
+        affiliation,<br>
+        corresponding to the appropriate address. Include the complete postal address for each affiliation, including
+        the country,<br>
+        and provide the email address for each author.</p>
+      <p style="color:gray;" ,style="font-size:16;"><b>Corresponding Author</b></p>
+      <p style="color:gray;">Specify individuals who are ready to manage communication throughout the entire peer review
+        process, publication,<br>
+        and post-publication. Include their email addresses, complete postal addresses, and telephone numbers with<br>
+        country and area codes.</p>
+    </div>
+
+    <p></p> <!-- Empty paragraph for space -->
+
+
+    <h5 style="font-size: 14px;">Abstract</h5>
+    <p style="color: grey;">Compose a brief and professional summary of the research within a 250-word limit. Clearly
+      state the research purpose, highlight
+      principal findings, and summarize key conclusions. Avoid including references in this standalone abstract.</p>
+
+    <h5 style="font-size: 14px;">Key Words</h5>
+    <p style="color: grey;">Immediately after the abstract, a maximum of 5 keywords, avoiding general and plural terms
+      and multiple concepts
+      (Ex:'and', 'of')<br>should be provided. Keywords should be presented. The keywords should be arranged in
+      alphabetical order.</p>
+    <h5 style="font-size: 14px;">Literature Review</h5>
+    <p style="color: grey;">This section should provide a sufficient evaluation of the theoretical/empirical literature
+      to identify the knowledge gaps.</p>
+    <h5 style="font-size: 14px;">Materials and Methods</h5>
+    <p style="color: grey;">This section should contain adequate and detailed information of all the procedures and
+      steps followed.</p>
+    <h5 style="font-size: 14px;">Results & Discussion</h5>
+    <p style="color: grey;">This section should describe the results and the outputs of the research work.</p>
+    <h5 style="font-size: 14px;">Conclusions</h5>
+    <p style="color: grey;">This section should clearly explain the important conclusions of the work highlighting its
+      significance, relevance and values.</p>
+    <h5 style="font-size: 14px;">Sub-division of the Article</h5>
+    <p style="color: grey;">This section should clearly explain the important conclusions of the work highlighting its
+      significance, relevance and values.</p>
+    <h5 style="font-size: 14px;">Tables and Figures</h5>
+    <p style="color: grey;">This section should clearly explain the important conclusions of the work highlighting its
+      significance, relevance and values.</p>
+    <div class="tables_figures_subtopics">
+      <p style="color:gray;" ,style="font-size:16;"><b>Table</b></p>
+      <p style="color:gray;">Number the tables sequentially using Arabic numerals, following the order of their
+        appearance in the text. Present each table<br>
+        on an individual page with double-spacing, accompanied by a brief descriptive title placed directly above it.
+        Include necessary<br>
+        footnotes below the tables. Authors are required to submit intricate tables in a print-ready format."</p>
+      <p style="color:gray;" ,style="font-size:16;"><b>Figures</b></p>
+      <p style="color:gray;">Prepare figures in a polished state suitable for publication. Assign Arabic numerals to
+        each figure and note the top and authors<br>
+        on the back of each. Lettering on illustrations should be of professional quality or generated through
+        high-resolution computer<br>
+        graphics, ensuring it is large enough for suitable reduction in publication. Avoid embedding figures within the
+        text. Acceptable<br>
+        digital artwork should have a minimum resolution of 300 dpi and be in eps or TIFF formats. For comprehensive
+        guidelines on<br>
+        creating electronic artwork, please refer to our website at
+        https://www.elsevier.com/artwork-and-media-instructions.</p>
+    </div>
+
+    <h5 style="font-size: 14px;">Acknowledgement</h5>
+    <p style="color: grey;">If the research is sponsored or supported by an organization, please indicate it.</p>
+    <h5 style="font-size: 14px;">Reference List</h5>
+    <div class="reference_list_subtopics">
+      <p style="color:gray;" ,style="font-size:16;"><b>Citation in Text</b></p>
+      <p style="color:gray;">Match citations in the text with entries in the reference list. Provide full references for
+        abstract citations.<br>
+        Avoid including unpublished. results or personal communications in the reference list, but if necessary, use
+        standard referencing with 'Unpublished Results' or '<br>
+        Personal Communication.' Note that 'in press' means the item has been accepted for publication.</p>
+      <p style="color:gray;" ,style="font-size:16;"><b>References</b></p>
+      <p style="color:gray;">When citing online sources, provide the complete URL and mention the date you last accessed
+        the information. If available, include<br>
+        additional details such as DOI, author names, publication dates, or source references. You can choose to list
+        web references separately<br>
+        or integrate them into the main reference list.</p>
+      <p style="color:gray;" ,style="font-size:16;"><b>Data Reference</b></p>
+      <p style="color:gray;">This journal encourages authors to mention and reference relevant datasets in their
+        manuscripts. When citing datasets, include author<br>
+        names, dataset title, data repository, version (if applicable), year, and a global persistent identifier in the
+        Reference List. Insert [dataset]<br>
+        before the reference to indicate it as a data reference; however, this identifier won't appear in the published
+        article.</p>
+    </div>
+
+
+
+    <body>
+
+
+      <h5 style="font-size: 16px;"><a style="color: #3F37C9;" href="https://example.com">Submission Preparation
+          Checklist</a></h5>
+      <!-- Two-column bullet list -->
+
+      <p><a style="color: #3F37C9;" href="https://example.com">Declaration & Verification </a></p>
+      <p><a style="color: #3F37C9;" href="https://example.com">Changes to Authorship</a></p>
+      <p><a style="color: #3F37C9;" href="https://example.com">Privacy Statement</a></p>
+      <p><a style="color: #3F37C9;" href="https://example.com">Plagiarism Policy</a></p>
+      <p><a style="color: #3F37C9;" href="https://example.com">Author’s Requirement to Sign in ORCID</a></li>
+    </body>
+
+</html>
---
+++ reviewPage/submissionPC.php
@@ -0,0 +1,129 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>SUJCS</title>
+  <link rel="stylesheet" href="submissionPC.css">
+</head>
+
+<body>
+  <nav class="navbar">
+    <div class="container">
+      <div class="logo">
+        <img src="D:/work/HTML/WEB_PROJECT/SUJCS_Web_Project/images/logo.png" alt="Logo">
+      </div>
+      <div class="menu">
+        <ul>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Call for Papers</a></li>
+          <li><a href="#">Review</a></li>
+          <li><a href="#">Guidelines</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </div>
+      <div class="buttons">
+        <a href="#" class="login-btn">Login</a>
+        <a href="#" class="signup-btn">Sign Up</a>
+      </div>
+    </div>
+  </nav>
+
+  <div class="title-container">
+    <h3>Find Articles</h3>
+    <div class="search-container">
+      <input type="text" placeholder="Search here">
+      <button type="submit">Search</button>
+    </div>
+  </div>
+</body>
+
+<body>
+  <h4 style="font-size: 16px;">Submission Preparation Checklist</h4>
+  <p style="color: grey;" ,style="text-align: justify;">Each manuscript should be submitted by e-mail, SUJCS@sab.ac.lk
+    attached to a covering letter confirming that it describes original<br>
+    research not submitted for publication or already published elsewhere.</p>
+  <p style="color: grey;" ,style="text-align: justify;">One author has been designated as the corresponding author with
+    contact details:</p>
+
+  <p style="color: grey;"><b>1. E-mail address<b></p>
+  <p style="color: grey;"><b>2. Full postal address<b></p>
+  <p style="color: grey;" ,style="text-align: justify;">All necessary files have been uploaded</p>
+  <h5 style="font-size: 14px;">Manuscript</h5>
+</body>
+
+<div class="subManuscript">
+  <ul>
+    <li style="color:grey;">Include Keywords</li>
+    <li style="color:grey;">All figures (include relevant captions)</li>
+    <li style="color:grey;">All tables (including titles, description, footnotes)</li>
+    <li style="color:grey;">Ensure all figure and table citations in the text match the files provided</li>
+    <li style="color:grey;">Indicate clearly if color should be used for any figures in print Graphical Abstracts /
+      Highlights files (where applicable)</li>
+    <li style="color:grey;">Supplemental files (where applicable)</li>
+  </ul>
+</div>
+<h5 style="font-size: 14px;">Further Considerations</h5>
+<div class="subFurtherConsideration">
+  <ul>
+    <li style="color:grey;">The submission file is in Microsoft Word, latex file format.</li>
+    <li style="color:grey;">Where available, URLs for the references have been provided.</li>
+    <li style="color:grey;">Where available, DOI s for the references have been provided.</li>
+    <li style="color:grey;">Manuscript has been 'spell checked' and 'grammar checked</li>
+    <li style="color:grey;">All references mentioned in the Reference List are cited in the text, and vice versa</li>
+    <li style="color:grey;">Permission has been obtained for use of copyrighted material from other sources (including
+      the Internet)</li>
+    <li style="color:grey;">A competing interests statement is provided, even if the authors have no competing interests
+      to declare</li>
+    <li style="color:grey;">Journal policies detailed in this guide have been reviewed</li>
+    <li style="color:grey;">Referee suggestions and contact details provided</li>
+  </ul>
+</div>
+<h5 style="font-size: 14px;">Declaration & Verification</h5>
+<div class="subDecleration">
+  <p style="color: grey;">When submitting an article, it means that the content has not been published before (except as
+    an abstract, lecture, or academic thesis).<br>
+    It is also not being considered for publication elsewhere, and all authors and relevant authorities where the work
+    was conducted have<br>
+    approved its publication.</p>
+</div>
+<h5 style="font-size: 14px;">Changes to Authorship</h5>
+<div class="subChanges">
+  <p style="color: grey;">Authors must carefully consider and finalize the list and order of authors when submitting
+    their manuscript. Any changes to the authorship<br>
+    list can only be made before the manuscript is accepted, and such changes require approval from the Journal Editor.
+    To request a change,<br>
+    the corresponding author must provide: (a) a reason for the alteration in the author list, and (b) written
+    confirmation (via email or letter) from<br>
+    all authors expressing their agreement with the addition, removal, or rearrangement. If an author is being added or
+    removed, confirmation <br>
+    from that specific author is also necessary.</p>
+</div>
+<h5 style="font-size: 14px;">Privacy Statement</h5>
+<div class="subPrivacy">
+  <p style="color: grey;">The information provided in this journal, including names and email addresses, will only be
+    used for the specific purposes mentioned in<br>
+    the journal. It will not be shared with any other party or used for any other reasons.</p>
+</div>
+<h5 style="font-size: 14px;">Plagiarism Policy</h5>
+<div class="subPlagiarism">
+  <p style="color: grey;">The Sabaragamuwa University Journal of Computer Science (SUJCS) strictly prohibits plagiarism,
+    data or figure manipulation, and the intentional<br>
+    dissemination of incorrect information. Additionally, any form of copyright infringement is not allowed. Before
+    undergoing peer review,<br>
+    all manuscripts will undergo a thorough plagiarism check.<br>
+  <p></p>
+  <p style="color: grey;">The similarity below 15% is acceptable by the Journal and a similarity of >25% is considered
+    as high percentage of plagiarism.</p>
+</div>
+<h5 style="font-size: 14px;">Author’s Requirement to Sign in ORCID</h5>
+<div class="subAuthor">
+  <p style="color: grey;">Authors need to sign up for ORCID and submit these to the journal in the time of submission of
+    the manuscript. More information on ORCID<br>
+    is available is at https://orcid.org.</p>
+</div>
+
+</html>
