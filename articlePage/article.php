dbd9169a3611d0700de7e2816dcb5f92fcf67487
Tharindu-Lakmal <126395249+Tharindu-Lakmal@users.noreply.github.com>
2024-03-04 23:41:38 +0530

    styles changes

--- articlePage/article.php
+++ articlePage/article.php
@@ -344,6 +344,8 @@ include('../function/commonFunction.php');
     <script src="../reuseComponents/navBar.js"></script>
     <script src="../reuseComponents/journal_article.js"></script>
 
+    <script src="../signIn/sign.js"></script>
+
     <!-- ionicon link -->
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
