3103ad58b9bb108a7adc66fd00448c2b944c653a
kalpani-ariyawanshe <skalpania@gmail.com>
2024-04-07 23:15:39 +0530

    Fix signin pop up not showing

--- signIn/sign.js
+++ signIn/sign.js
@@ -4,46 +4,39 @@ const main = document.querySelector("main");
 const bullets = document.querySelectorAll(".bullets span");
 const images = document.querySelectorAll(".image");
 
-
-
-
 inputs.forEach((inp) => {
-  inp.addEventListener("focus", () => {
-    inp.classList.add("active");
-  });
-  inp.addEventListener("blur", () => {
-    if (inp.value != "") return;
-    inp.classList.remove("active");
-  });
+	inp.addEventListener("focus", () => {
+		inp.classList.add("active");
+	});
+	inp.addEventListener("blur", () => {
+		if (inp.value != "") return;
+		inp.classList.remove("active");
+	});
 });
 
 toggle_btn.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    main.classList.toggle("sign-up-mode");
-  });
+	btn.addEventListener("click", () => {
+		main.classList.toggle("sign-up-mode");
+	});
 });
 
-
-
-
 function moveSlider() {
-    let index = this.dataset.value;
-  
-    let currentImage = document.querySelector(`.img-${index}`);
-    images.forEach((img) => img.classList.remove("show"));
-    currentImage.classList.add("show");
-  
-    const textSlider = document.querySelector(".text-group");
-    textSlider.style.transform = `translateY(${-(index - 1) * 2.2}rem)`;
-  
-    bullets.forEach((bull) => bull.classList.remove("active"));
-    this.classList.add("active");
-  }
-  
-  bullets.forEach((bullet) => {
-    bullet.addEventListener("click", moveSlider);
-  });
+	let index = this.dataset.value;
 
+	let currentImage = document.querySelector(`.img-${index}`);
+	images.forEach((img) => img.classList.remove("show"));
+	currentImage.classList.add("show");
+
+	const textSlider = document.querySelector(".text-group");
+	textSlider.style.transform = `translateY(${-(index - 1) * 2.2}rem)`;
+
+	bullets.forEach((bull) => bull.classList.remove("active"));
+	this.classList.add("active");
+}
+
+bullets.forEach((bullet) => {
+	bullet.addEventListener("click", moveSlider);
+});
 
 ////////////////////////////////////////////////////
 // const mainSection = document.querySelectorAll('main');
@@ -51,15 +44,6 @@ const displayBtn = document.querySelector("[data-sign]");
 const closeBtn = document.querySelector("[data-close]");
 const sign_main = document.querySelector("[data-sign-main]");
 
+displayBtn.addEventListener("click", () => (sign_main.style.display = "flex"));
 
-displayBtn.addEventListener("click", () =>
-
-  sign_main.classList.add("avtive")
-
-);
-
-closeBtn.addEventListener("click", () =>
-
-  sign_main.classList.remove("avtive")
-  
-);
+closeBtn.addEventListener("click", () => (sign_main.style.display = "none"));
